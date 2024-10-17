f121c3f645620c4e4c3540d3499afb12c61355b0
Gustavo Nunes <gustavompn06@gmail.com>
2024-10-17 18:55:59 +0000

    att gitignore, função do botão

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,29 @@ import React, { useState } from 'react';
 
 function Home() {
 
-const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-function Action() {
-    setMessage('Eu te amo');
-};
+    function Action() {
+        if (message == 'Eu te amo'){
+            setMessage('')
+        }
+        else{
+            setMessage('Eu te amo');
+        }
+    };
 
-return (
-    <div>
-        <h1>Olá</h1>
+    return (
+        <div>
+            <h1>Olá</h1>
 
-        <button onClick={Action}>Clique Aqui</button>
-        <br></br>
-        <br></br>
-        <div>{message}</div>
-    </div>
+            <button onClick={Action}>Clique Aqui</button>
+            <br></br>
+            <br></br>
+            <div>{message}</div>
+
+        </div>
     );
+
 }
 
 export default Home;
