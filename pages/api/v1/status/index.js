0bafa351493d46106b9a42bb5cdfd0398381adbb
Gustavo Nunes <gustavompn06@gmail.com>
2026-06-19 06:26:16 -0300

    feat: database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,13 @@
-function status(request, response) {
-    response.status(200).json(
-        {
-             status: '200',
-             message: "são média"
-        }
-    );
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const test = await database.query("SELECT 1 + 1;")
+  // console.log("test")
+  console.log(test.rows)
+
+  response.status(200).json({
+    message: "são média",
+  });
 }
 
 export default status;
